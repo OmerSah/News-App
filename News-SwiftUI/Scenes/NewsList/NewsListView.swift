efbcfc23506f67266f3275cfe28f07ce89e5d88b
Ömer Faruk Şahin <omerfaruksahiin@gmail.com>
2022-08-17 22:05:38 +0300

    Fix issues

--- News-SwiftUI/Scenes/NewsList/NewsListView.swift
+++ News-SwiftUI/Scenes/NewsList/NewsListView.swift
@@ -23,6 +23,12 @@ struct NewsListView: View {
     
     @State private var showSafari = false
     
+    @State private var fromDate = Date()
+    
+    private let minimumDate: Date = DateFormatter.filterDateFormat.date(from: "2022-07-20")!
+    private let maximumDate: Date = DateFormatter.filterDateFormat.date(from: "2022-08-15")!
+    
+    @State private var toDate = Date()
     var body: some View {
         NavigationView {
             List(viewModel.news) {  article in
@@ -83,6 +89,7 @@ struct NewsListView: View {
                         query: self.searchText
                     )
                 }.offset(y: UIScreen.main.bounds.height * -0.2)
+            }
             }
         }
         
--- News-SwiftUI/Utilities/Constants/APIConstants.swift
+++ News-SwiftUI/Utilities/Constants/APIConstants.swift
@@ -9,8 +9,8 @@ import Foundation
 
 extension Constants {
     struct API {
-        //static let apiKey = "e8ed2d05ab9842a39b8207a53d8eb3bb"
-        static let apiKey = "c83dad2c1dc54a6d80dba31d8657b662"
+        static let apiKey = "e8ed2d05ab9842a39b8207a53d8eb3bb"
+        //static let apiKey = "c83dad2c1dc54a6d80dba31d8657b662"
         
         static let host = "newsapi.org"
     }
